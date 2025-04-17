78d58aabb92a4e2e6dadbd1348f308ba8ddf25f9
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-04-17 09:23:11 +0300

    Server Sent Event Exercise

---
+++ 02-SeverSentEvents/02-ServerSentEvents copy.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+
+// SSE endpoint
+app.get("/events", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+
+
+  let counter = 0;
+
+  const intervalId = setInterval(() => {
+    counter++;
+    res.write(`data: ${JSON.stringify({ message: "Merhaba", count: counter })}\n\n`);
+  }, 1000);
+
+  // Bağlantı kapanırsa interval temizlenmeli
+  req.on("close", () => {
+    clearInterval(intervalId);
+    res.end();
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`SSE server listening at http://localhost:${PORT}`);
+});
